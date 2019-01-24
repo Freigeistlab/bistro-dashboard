84a52f2c45f011bf94918bbe5eb2a27621f21963
Julius Rudolph <julius.rudolph@student.hpi.de>
2019-01-24 16:21:01 +0100

    updates correct host

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,5 +5,5 @@ export const ingredientTypes = {
   DECORATION: 3
 };
 
-export const webSocketURI = "ws://localhost:5678";
-export const apiURI = "http://localhost:5000";
+export const webSocketURI = "ws://192.168.1.105:5678";
+export const apiURI = "http://192.168.1.105:5000";
